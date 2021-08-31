716e225461931a3cd3bcbdc762dbf18b60fdbcc3
Rafèq <87463084+Rafeqm@users.noreply.github.com>
2021-08-31 10:21:31 +0700

    Update

--- kuda.py
+++ kuda.py
@@ -1,15 +1,21 @@
 import os
+import json
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+def get_prefix(client, message):
+    with open('configurasi.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    return prefixes[str(mesage.guild.id)]
 
 #======KAMUS UTAMA======#
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(
-    command_prefix="?",
+    command_prefix=get_prefix(),
     case_insensitive=True,
     intents=intents,
     allowed_mentions=discord.AllowedMentions(
@@ -25,12 +31,33 @@ client = commands.Bot(
 async def on_ready():
     print(f'I now live as {client.user} to serve you, My Lord!')
 
+@client.event
+async def on_guild_join(guild:discord.Guild):
+    with open('configurasi.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    prefixes[str(guild.id)] = '?'
+
+    with open('configurasi.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild:discord.Guild):
+    with open('configurasi.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    prefixes.pop[str(guild.id)]
+
+    with open('configurasi.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
 """
 Load a single cog in cogs folder:
 client.load_extension('cogs.CogFileName')
 
 Unload a single cog in cogs folder:
 client.unload_extension('cogs.CogFileName')
+
 """
 
 # --Load all cogs in cogs folder
